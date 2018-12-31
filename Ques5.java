266d62c606b94c8f5e350b60321be1347b5173c2
Yashawant <yashawant.nita@gmil.com>
2018-12-31 09:04:32 +0530

    Modified

--- Ques5.java
+++ Ques5.java
@@ -4,17 +4,15 @@
 */
 import java.util.Scanner;
 
-public class Ques5
-{
-    public static void main(String[] args)
-    {
+public class Ques5 {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int sum = 0;
         System.out.println("Enter a number: ");
         while (scanner.hasNextInt()) {    /*For unspecified number of inputs*/
-            System.out.println("Enter a number to add and other character to print sum: ");
-            sum+=scanner.nextInt();
+            System.out.println("Enter a number to add or other character to print sum: ");
+            sum += scanner.nextInt();
         }
-        System.out.println("Sum = "+sum);
+        System.out.println("Sum = " + sum);
     }
 }
--- Ques9.java
+++ Ques9.java
@@ -9,7 +9,7 @@ public class Ques9 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a string: ");
+        System.out.println("Enter a string:");
         String str1 = sc.next();
         String str2 = "";
 
